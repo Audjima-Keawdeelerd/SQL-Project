a397a0ff5de895475c2ed090c6056168eb65f050
Audjima-Keawdeelerd <audjima2554@gmail.com>
2026-02-19 20:07:12 +0700

    add SQL project

---
+++ create table.sql
@@ -0,0 +1,30 @@
+
+CREATE TABLE Customers (
+	CustomerID INT PRIMARY KEY IDENTITY(1,1),
+	FirstName NVARCHAR(50),
+	LastName NVARCHAR(50),
+	Email NVARCHAR(100),
+	Phone NVARCHAR(50),
+	Address NVARCHAR(255),
+	City NVARCHAR(50),
+	State NVARCHAR(50),
+	ZipCode NVARCHAR(50),
+	Country NVARCHAR(50),
+	CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE Products (
+	ProductID INT PRIMARY KEY IDENTITY(1,1),
+	ProductName NVARCHAR(100),
+	CategoryID INT,
+	Price DECIMAL(10,2),
+	Stock INT,
+	CreatedAt DATETIME DEFAULT GETDATE()
+);
+CREATE TABLE Orders (
+	OrderId INT PRIMARY KEY IDENTITY(1,1),
+	CustomerId INT,
+	OrderDate DATETIME DEFAULT GETDATE(),
+	TotalAmount DECIMAL(10,2),
+	FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
